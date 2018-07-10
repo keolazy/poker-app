a76b718a28c1bc7791a57ae635adb0cdb8be1feb
Nathan Keolasy <keolazy1@gmail.com>
2018-07-10 18:22:31 -0500

    threw in RBC drag&drop starter code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
+// Stock Imports
 import React, { Component } from 'react';
-// logo.svg contains premade code for the spinning icon.
-import logo from './poker-icon.png';
+// React-Big-Calendar stuff.
+
 import './App.css';
+// import "react-big-calendar/lib/addsons/dragAndDrop/styles.css";
+// import "react-big-calendar/lib/css/react-big-calendar.css";
+import logo from './poker-icon.png';
+
+Calendar.setLocalizer(BigCalendar.momentLocalizer(moment))
+
+const DnDCalendar = withDragAndDrop(Calendar);
+
 
 class App extends Component {
+  state = {
+    events: [
+      {
+        start: new Date(),
+        end: new Date(moment().add(1, "days")),
+        title: "Some title"
+      }
+    ]
+  };
+
+  onEventResize = (type, { event, start, end, allDay }) => {
+    this.setState(state => {
+      state.events[0].start = start;
+      state.events[0].end = end;
+      return { events: state.events };
+    });
+  };
+
+  onEventDrop = ({ event, start, end, allDay }) => {
+    console.log(start);
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Poker-App</h1>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Start adding Components in ./src/App.js
+          To get started, Add Components appropriately to <code>src/App.js</code> and save to reload.
         </p>
+        <DnDCalendar
+          defaultDate={new Date()}
+          defaultView="month"
+          events={this.state.events}
+          onEventDrop={this.onEventDrop}
+          onEventResize={this.onEventResize}
+          resizable
+          style={{ height: "100vh" }}
+        />
       </div>
     );
   }
+
 }
 
 export default App;
--- src/components/User-Signin.js
+++ src/components/User-Signin.js
@@ -1,6 +1,6 @@
-import React from "react";
+// import React from "react";
 
 
 
 
-export default User-Signin;
+// export default User-Signin;
