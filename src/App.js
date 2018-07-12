82b94c5a2b7bc06bc0c5b18883fa133093a359b6
Nathan Keolasy <keolazy1@gmail.com>
2018-07-12 01:30:03 -0500

    added some placeholder divs for where the components will be in application

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import logo from './poker-icon.png';
+import Jumbotron from './components/Jumbotron'
+
+
 
 // Implement Navigation Routing. 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
@@ -13,8 +16,25 @@ class App extends Component {
           <h1 className="App-title">Poker-App</h1>
         </header>
         <p className="App-intro">
-          To get started, Add Components appropriately to <code>src/App.js</code> and save to reload.
+          Add Components appropriately to <code>src/App.js</code> and save to reload.
         </p>
+        <div className="tempBox">
+           <h1>Jumbotron goes here.</h1>
+          <Jumbotron image={this.props.image} />
+        </div>
+        <div className="tempBox">
+           <h1>Calendar Component goes here.</h1>
+        </div>
+        <div className="tempBox">
+          <div className="row">
+            <div className="col-md-6">
+              <h1>User-Card Profile Component goes here.</h1>
+            </div>
+            <div className="col-md-6">
+              <h1>Other Dashboard Component goes here.</h1>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
