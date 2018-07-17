96dab99adba30e2c7c969d519ceab8a0976d22dd
Nathan Keolasy <keolazy1@gmail.com>
2018-07-17 11:05:20 -0500

    installed pg bodyparser

--- routes/accounts-routes.js
+++ routes/accounts-routes.js
@@ -1,12 +1,32 @@
 // 
 const express = require('express')
-const router = express.Router()
+// const parseurl = require('parseurl');
+const bodyParser = requrie('body-parser');
+const path = require('path');
+const expressValidator = require('express-validator');
+const knex = require('knex');
 const db = require('../db/knex')
+const app = express();
 
+
+const router = express.Router()
+
+const app = express();
+
+// Root Directory
 router.get('/', async (req, res, next) => {
   const users = await db('users')
-  res.json(books)
+  console.log("you did it")
+  res.json(users)
 });
+
+// Get All "Signatures"
+router.get('/api/users', funtion(req,res) {
+
+}
+
+// Post New Users
+app.post('/api/poker')
 
 
 module.exports = router
